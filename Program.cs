6f953d6522bbca5ddfce01c5f91eeb6bd23ab161
Nemenisi <71167912+Nemenisi@users.noreply.github.com>
2022-02-12 14:11:42 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace socket_c__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
